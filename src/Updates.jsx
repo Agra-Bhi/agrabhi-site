47289afd6f26930f2d3ece15fba53b41ee254e40
Nathan Sharma <154014801+nathan-sharma@users.noreply.github.com>
2026-09-17 13:20:00 +0000

    9/16 update removed

--- src/Updates.jsx
+++ src/Updates.jsx
@@ -1437,21 +1437,10 @@ const MONTHS_DATA = [
         content: (
             <div className="text-slate-500 pt-1">
 
-     <p className="text-slate-400 leading-relaxed mb-4">
-                    <span className="inline-block w-2 h-2 bg-white rounded-full mr-3 mb-[2px]"></span>
-                    September 16th, 2026: The penetration test on September 1st only worked when the rover had a 20-pound dumbbell on it (because of the pushback force). The rover needs to anchor itself to penetrate the ground without added weight, so we decided to use the support rods as anchors ( the rods on the sides of the tower which prevent the sensor from swaying left to right as it penetrates the ground). They will be modified so that they are metal, the top of the rod is hex-shaped, and the bottom is an auger. This way, the rover can experience more pushback force without lifting itself off the ground.
-                    </p>
+    
             <div className="flex flex-col md:flex-row gap-6 items-stretch w-full mb-4">
     {/* Image Figure 1 */}
-    <figure className="flex flex-col flex-1">
-        <div className="w-full aspect-video overflow-hidden">
-            <img
-                src={supportroddesign1}
-                alt="Support rod design"
-                className="w-full h-full object-contain"
-            />
-        </div>
-    </figure>
+
     </div>
      <p className="text-slate-400 leading-relaxed mb-4">
                     <span className="inline-block w-2 h-2 bg-white rounded-full mr-3 mb-[2px]"></span>
